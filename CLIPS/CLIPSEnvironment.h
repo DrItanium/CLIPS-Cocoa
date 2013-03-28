60453abb0eb2ae70aa14348d75bb34f580f35457
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-28 16:16:49 -0700

    Defined the initial header for the CLIPSEnvironment class

---
+++ CLIPS/CLIPSEnvironment.h
@@ -0,0 +1,39 @@
+//
+//  CLIPSEnvironment.h
+//  CLIPS
+//
+//  Created by Joshua Scoggins on 3/28/13.
+//  Copyright (c) 2013 Joshua Scoggins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLIPSEnvironment : NSObject {
+    void* ptr;
+    Boolean preExistingEnvironment;
+}
+-(id) init;
+-(id) initWithPreExistingEnvironment: (void*) env;
+-(void) dealloc;
+-(void) run: (NSInteger) duration;
+-(void) batchStar: (NSString*) path;
+-(void) loadStar: (NSString*) path;
+-(void) batch: (NSString*) path;
+-(void) load: (NSString*) path;
+-(void) build: (NSString*) input;
+-(void) eval: (NSString*) input;
+-(void) halt;
+-(void) assertString: (NSString*) string;
+-(void) makeInstanceFromString: (NSString*) string;
+-(void) makeInstancesFromString: (NSString*) instances;
+-(void) printRouter: (NSString*) router withMessage: (NSString*) message;
+-(void) printMessage: (NSString*) message;
+-(void) printError: (NSString*) message;
+-(void) addFunction: (NSString*) name returnType: (char) retType fnPtr: (void*) fPtr fnName: (NSString*) fnName;
+-(void) addFunction2: (NSString*) name returnType: (char) retType fnPtr: (void*) fPtr fnName: (NSString*) fnName fnArgs: (NSString*) fnArgs;
+-(NSInteger) argCount: (NSString*) functionName;
+-(Boolean) argCountCheck: (NSString*) functionName qualifier: (NSInteger) qualifier count: (NSInteger) aCount;
+-(Boolean) argTypeCheck: (NSString*) functionName type: (NSInteger) qType targetIndex: (NSInteger) offset
+            dataStorage: (id) storage;
+
+@end

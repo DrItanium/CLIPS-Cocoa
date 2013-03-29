cfe6ece70cb1433af7d7a08e2d738163fe5e89c3
Joshua Scoggins <Theoretically.x64@gmail.com>
2013-03-28 19:24:40 -0700

    Added NSLogRouters.h
    This header is designed to expose NSLog to clips

---
+++ CLIPS/NSLogRouter.h
@@ -0,0 +1,14 @@
+//
+//  NSLogRouter.h
+//  CLIPS
+//
+//  Created by Joshua Scoggins on 3/28/13.
+//  Copyright (c) 2013 Joshua Scoggins. All rights reserved.
+//
+
+#ifndef CLIPS_NSLogRouter_h
+#define CLIPS_NSLogRouter_h
+
+void RegisterNSLogRouters(void* theEnv);
+
+#endif
